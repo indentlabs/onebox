11ed440e3e745b18144a207ad37486aaf159f3c4
drusepth <drusepth@gmail.com>
2022-05-24 15:02:22 -0700

    implement engine

--- lib/onebox/engine/notebook_page_onebox.rb
+++ lib/onebox/engine/notebook_page_onebox.rb
@@ -15,58 +15,13 @@ module Onebox
       def data
         og = get_opengraph
 
-        max_length = 250
-
-        display_path = extract_path(og.url, max_length)
-        display_description = clean_description(og.description, og.title, max_length)
-
-        title = og.title
-
-        fragment = Addressable::URI.parse(url).fragment
-        if fragment
-          fragment = Addressable::URI.unencode(fragment)
-
-          if html_doc.css('.Box.md')
-            # For links to markdown docs
-            node = html_doc.css('a.anchor').find { |n| n['href'] == "##{fragment}" }
-            subtitle = node&.parent&.text
-          elsif html_doc.css('.Box.rdoc')
-            # For links to rdoc docs
-            node = html_doc.css('h3').find { |n| n['id'] == "user-content-#{fragment.downcase}" }
-            subtitle = node&.css('text()')&.first&.text
-          end
-
-          title = "#{title} - #{subtitle}" if subtitle
-        end
-
         {
           link: url,
           image: og.image,
-          title: Onebox::Helpers.truncate(title, 250),
-          path: display_path,
-          description: display_description,
+          title: Onebox::Helpers.truncate(og.titletitle, 250),
+          description: Onebox::Helpers.truncate(og.description, 400),
           favicon: get_favicon
         }
-      end
-
-      def extract_path(root, max_length)
-        path = url.split('#')[0].split('?')[0]
-        path = path["#{root}/tree/".length..-1]
-
-        return unless path
-
-        path.length > max_length ? path[-max_length..-1] : path
-      end
-
-      def clean_description(description, title, max_length)
-        return unless description
-
-        desc_end = " - #{title}"
-        if description[-desc_end.length..-1] == desc_end
-          description = description[0...-desc_end.length]
-        end
-
-        Onebox::Helpers.truncate(description, max_length)
       end
     end
   end
